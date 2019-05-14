ca538ae3ae56de531c69741d8bcb4758dba80ca8
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-05-14 15:48:09 +0100

    Simple Hello World Example in Python added
    Simple Hello World Example in Python added

---
+++ qiskit/tutorials/00. intro/a. hello-world/hello-world.py
@@ -0,0 +1,20 @@
+# Quantum Computing, using QisKit
+
+# QisKit Tutorials
+
+# Author:
+# - Ruben Andre Barreiro
+
+# MSc. of Computer Science and Engineering
+
+# Department of Informatics
+
+# Faculty of Sciences and Technology of
+# New University of Lisbon (FCT NOVA | FCT/UNL), Portugal
+
+# Chapter 0 - Intro
+# Lesson A) Hello World
+
+# Hello World in Python
+
+print("Hello World!!!")
